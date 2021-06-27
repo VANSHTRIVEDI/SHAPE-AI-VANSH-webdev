f3e49f9c73e67d2275a06093b9fc83cd1703b345
VANSHTRIVEDI <86413513+VANSHTRIVEDI@users.noreply.github.com>
2021-06-28 02:31:55 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright By VAOM cooperation @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+function Info() {
+  return (
+    <div class="note">
+      <h1>VAOM COORPERATION</h1>
+      <h1>Founder's</h1>
+      <h3> Vansh Trivedi</h3>
+      <h3>Om Singh</h3>
+
+      <p>
+        VAOM Cooperation is a groud of startup which was founded by Vansh
+        Trivedi and Om Singh It was established in 2014 amd has its headquater
+        in Raebareli.It consist of startups like weber,womb,anya etc
+      </p>
+    </div>
+  );
+}
+
+export default Info;
